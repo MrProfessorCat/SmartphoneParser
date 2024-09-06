71892554e15a9f2cd50db4c22be03a489308470f
Andrey <professor.cat@inbox>
2024-09-07 00:55:12 +0300

    added main spider

--- phone_parser/phone_parser/pipelines.py
+++ phone_parser/phone_parser/pipelines.py
@@ -1,13 +1,17 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import pandas as pd
 
 
 class PhoneParserPipeline:
+    def open_spider(self, spider):
+        self.phones_op_systems = []
+
     def process_item(self, item, spider):
+        self.phones_op_systems.append(
+            item.get('phone_os')
+        )
         return item
+
+    def close_spider(self, spider):
+        df = pd.DataFrame(self.phones_op_systems, columns=('OS version',))
+        print(df.value_counts())
+        df.value_counts().to_csv('res.csv', encoding='utf-8')
--- phone_parser/phone_parser/spiders/phones_spider.py
+++ phone_parser/phone_parser/spiders/phones_spider.py
@@ -1,24 +1,68 @@
-from typing import Iterable
 import scrapy
+import urllib
+
+from phone_parser.items import PhoneParserItem
+
+BASE_URL = 'https://www.ozon.ru'
+
+MAX_PHONES_TO_PARSE = 30
+
+SMARTPHONE = 'Смартфон'
+MAIN_CHARACTERISTICS = 'Основные'
+OPERATING_SYSTEM = 'Операционная система'
 
 
 class PhonesSpider(scrapy.Spider):
     name = 'phones_spider'
-
-    def start_requests(self) -> Iterable[scrapy.Request]:
-        url = 'https://www.ozon.ru/category/telefony-i-smart-chasy-15501/?sorting=rating' # noqa E501
-        yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ['https://www.ozon.ru/category/telefony-i-smart-chasy-15501/?sorting=rating&type=49659',] # noqa E501
+    phones_num = 0
 
     def parse(self, response):
-        for product in response.css('div.widget-search-result-container div'):
-            print(product)
-            yield {
-                'product_type': product.css(
-                    'span.tsBody400Small font::text').get(),
-                'url': product.css('a.l6j_23::attr(href)').get(),
-            }
+        products = response.css(
+            'div.widget-search-result-container div.o2j_23'
+        )
+        for product in products:
+            if self.phones_num == MAX_PHONES_TO_PARSE:
+                return
+            product_type = product.css(
+                'span.tsBody400Small font::text').get()
+            url = product.css('a.tile-hover-target::attr(href)').get()
+            if product_type == SMARTPHONE:
+                yield response.follow(url, callback=self.parse_phone)
+                self.phones_num += 1
 
-        # next_page = response.css(
-        #     'a.e3q.b2113-a0.b2113-b6.b2113-b1::attr(href)').get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, callback=self.parse)
+        next_page = response.css(
+            'a.e3q.b2113-a0.b2113-b6.b2113-b1::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(
+                urllib.parse.urljoin(BASE_URL, next_page),
+                callback=self.parse
+            )
+
+    def parse_phone(self, response):
+        """Parsing page with phone information"""
+        # getting block with characteristics
+        characteristics = response.css('div#section-characteristics')
+        # getting all blocks in characteristics block
+        blocks = characteristics.css('div.ku6_27')
+        main_block = None
+        # searching for block "Основные"
+        for block in blocks:
+            block_title = block.css('div.uk6_27::text').get()
+            if block_title == MAIN_CHARACTERISTICS:
+                main_block = block
+                break
+        if main_block:
+            # getting all characteristics from block "Основные"
+            main_characteristics = main_block.css('dl.u9k_27')
+            # searching for characteristic with name "Операционная система"
+            for characteristic in main_characteristics:
+                character_name = characteristic.css('span.k9u_27::text').get()
+                if character_name == OPERATING_SYSTEM:
+                    operating_system = characteristic.css('a::text').get()
+                    if operating_system:
+                        yield PhoneParserItem({'phone_os': operating_system})
+                    else:
+                        with open('errors_1.txt', 'a') as erfile:
+                            erfile.write(response.url + '\n')
+                    break
